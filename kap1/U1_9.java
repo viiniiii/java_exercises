ab0e11d61d7dce73bfbbf4fb15a20de449e6b556
viiniii <123756468+viiniiii@users.noreply.github.com>
2023-11-21 13:37:13 +0100

    Delete kap1 directory

--- kap1/U1_9.java
+++
@@ -1,9 +0,0 @@
-
-public class U1_9 {
-    public static void main(String[] args) {
-        double gjeresia = 4.5;
-        double lartesia = 7.9;
-        System.out.println("siperfaqja eshte: " + (float)(gjeresia * lartesia));
-
-    }
-}
